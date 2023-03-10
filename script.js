2cad8554e56160bd693d01237b0ecb3f4eace7d6
Alexander Dmitriev <test@example.com>
2023-03-10 19:50:46 +0300

    Init

---
+++ script.js
@@ -0,0 +1,23 @@
+let loginBtn = document.querySelector('.header-login-btn')
+let loginModal = document.querySelector('.login-modal')
+loginBtn.onclick = function () {
+  loginModal.classList.add('show')
+  document.body.classList.add("stop-scrolling")
+}
+
+loginModal.onsubmit = function(event) {
+  event.preventDefault()
+  console.log(event.target.username.value)
+  console.log(event.target.password.value)
+  let username = event.target.username.value
+  let password = event.target.password.value
+  let userInfoElem = document.querySelector('.userinfo')
+  userInfoElem.innerHTML = `<span>${username}</span><button onclick="logout()">Logout</button>`
+  loginModal.classList.remove('show')
+  document.body.classList.remove("stop-scrolling")
+}
+
+function logout() {
+  let userInfoElem = document.querySelector('.userinfo')
+  userInfoElem.innerHTML = '<button class="header-login-btn">Login</button>'
+}
